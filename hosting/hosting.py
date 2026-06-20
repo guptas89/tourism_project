b7ec0aba0bf81a914fe20bf1e75f394285f32ce0
Siddharth Gupta <siddharthgupta2010@gmail.com>
2026-06-20 19:17:05 +0530

    Adding data scripts

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -1,11 +1,25 @@
-from huggingface_hub import HfApi
+from huggingface_hub import HfApi, create_repo
+from huggingface_hub.utils import RepositoryNotFoundError
 import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
+repo_id = "guptas89/tourism-space"
+repo_type = "space"
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, space_sdk="streamlit", private=False)
+    print(f"Space '{repo_id}' created.")
+
+# Step 2: Upload files to the space
 api.upload_folder(
     folder_path="deployment",     # the local folder containing your files
-    repo_id="guptas89/tourism-space",          # the target repo
-    repo_type="space",                      # dataset, model, or space
+    repo_id=repo_id,          # the target repo
+    repo_type=repo_type,                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
 
