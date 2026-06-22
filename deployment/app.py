7b2a9ada96085a1c2c058a0459073856cfdbc7d4
Siddharth Gupta <siddharthgupta2010@gmail.com>
2026-06-22 09:23:48 +0530

    Adding comments

--- deployment/app.py
+++ deployment/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from huggingface_hub import hf_hub_download
 
-# Download and load the trained model
+# Download and load the trained model.
 model_path = hf_hub_download(
     repo_id="guptas89/tourism-prediction-model",
     filename="best_tourism_model_v1.joblib"
